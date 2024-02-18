036844a177a671adc370ee63603787ab25e5ac6f
Thomas Marsh <thomas.marsh2@gmail.com>
2024-02-17 20:01:00 -0500

    Update bench

--- benches/ttt.rs
+++ benches/ttt.rs
@@ -1,18 +1,22 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use mcts::games::ttt;
+use mcts::strategies::mcts::util;
+use mcts::strategies::mcts::MctsStrategy;
 use mcts::strategies::mcts::TreeSearch;
+use mcts::strategies::Search;
 
-type TicTacToeTS = TreeSearch<ttt::TicTacToe>;
+type TicTacToeTS = TreeSearch<ttt::TicTacToe, util::Ucb1>;
 
 fn ponder(c: &mut Criterion) {
     let mut group = c.benchmark_group("ttt");
     for n in [250, 500, 750, 1000] {
         group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
             b.iter(|| {
-                let mut ts = TicTacToeTS::new();
-                ts.set_max_rollouts(n);
-                ts.choose_move(&ttt::HashedPosition::new());
+                let mut ts =
+                    TicTacToeTS::default().strategy(MctsStrategy::default().max_iterations(n));
+
+                ts.choose_action(&ttt::HashedPosition::new());
             });
         });
     }
